18a62a44f4ceb7e28f3a2076a8465796dbbfc818
Max Krieger <themaxaxis@gmail.com>
2017-04-22 15:40:47 -0400

    Fixed refresh bug

--- app/scripts.babel/background.js
+++ app/scripts.babel/background.js
@@ -1,47 +1,51 @@
-"use strict";
+'use strict';
 
 let state = {
-	urls: [],
-	enabled: true
+    urls: [],
+    enabled: true
 };
 
 chrome.runtime.onMessage.addListener(
-	function(request, sender, callback){
-		if(request.directive == "togglePigeon") {
-			state.enabled = !state.enabled;
-			chrome.storage.sync.set({"state": state});
-			callback(state.enabled);
-		}
-		else if(request.directive == "addSite") {
-			state.urls.push(request.site);
-			chrome.storage.sync.set({"state": state});
-			callback();
-		}
-		else if(request.directive == "removeSite") {
-			state.urls.splice(state.urls.indexOf(request.site), 1);
-			chrome.storage.sync.set({"state": state});
-			callback();
-		}
-	}
+    function(request, sender, callback){
+        if(request.directive == 'togglePigeon') {
+            state.enabled = !state.enabled;
+            chrome.storage.sync.set({'state': state});
+            callback(state.enabled);
+        }
+        else if(request.directive == 'addSite') {
+            state.urls.push(request.site);
+            chrome.storage.sync.set({'state': state});
+            callback();
+        }
+        else if(request.directive == 'removeSite') {
+            state.urls.splice(state.urls.indexOf(request.site), 1);
+            chrome.storage.sync.set({'state': state});
+            callback();
+        }
+        else if(request.directive == 'test') {
+            console.log('yes');
+            callback();
+        }
+    }
 );
 
 let matchURL = (url) => {
-	url = url.toLowerCase();
-	let stripped = url.match(/\:\/\/(www\.)?((.[^/]*)\.(.[^/]*))\//);
-	return (stripped && state.urls.indexOf(stripped[2]) > -1);
+    url = url.toLowerCase();
+    let stripped = url.match(/\:\/\/(www\.)?((.[^/]*)\.(.[^/]*))\//);
+    return (stripped && state.urls.indexOf(stripped[2]) > -1);
 };
 
 chrome.webRequest.onBeforeRequest.addListener((tab) => {
-	if(matchURL(tab.url) && state.enabled) {
-		return {
-			redirectUrl: chrome.extension.getURL("blocked.html")
-		};
-	}
+    if(matchURL(tab.url) && state.enabled) {
+        return {
+            redirectUrl: chrome.extension.getURL('blocked.html')
+        };
+    }
 }, {
-	urls: ["<all_urls>"]
-}, ["blocking"]);
+    urls: ['<all_urls>']
+}, ['blocking']);
 
-chrome.storage.sync.get("state", (data) => {
-	if(data.state) state = data.state;
-	else chrome.storage.sync.set({"state": state});
+chrome.storage.sync.get('state', (data) => {
+    if(data.state) state = data.state;
+    else chrome.storage.sync.set({'state': state});
 });
--- app/scripts.babel/popup.js
+++ app/scripts.babel/popup.js
@@ -8,47 +8,49 @@ let blockInput = document.querySelector("#blockInput");
 let sitesEl = document.querySelector("#sites");
 
 toggleButton.onclick = () => {
-	chrome.runtime.sendMessage({directive: "togglePigeon"}, (response) => {
-		renderToggle(response);
-	});
+    chrome.runtime.sendMessage({directive: "togglePigeon"}, (response) => {
+        renderToggle(response);
+    });
 };
 
 let renderToggle = (toggled) => {
-	toggleButton.innerHTML = (toggled ? "Turn Off" : "Turn On");
-	toggleButton.style.backgroundColor = (toggled ? "#000000" : "#FFFFFF");
-	toggleButton.style.color = (toggled ? "#FFFFFF" : "#000000");
+    toggleButton.innerHTML = (toggled ? "Turn Off" : "Turn On");
+    toggleButton.style.backgroundColor = (toggled ? "#000000" : "#FFFFFF");
+    toggleButton.style.color = (toggled ? "#FFFFFF" : "#000000");
 };
 
 blockListButton.onclick = () => {
-	mainView.style.display = "none";
-	blockView.style.display = "";
-	chrome.storage.sync.get("state", (data) => {
-		sitesEl.innerHTML = "";
-		let sites = data.state.urls;
-		sites.forEach(createEl);
-	});
+    mainView.style.display = "none";
+    blockView.style.display = "";
+    chrome.storage.sync.get("state", (data) => {
+        sitesEl.innerHTML = "";
+        let sites = data.state.urls;
+        sites.forEach(createEl);
+    });
 };
 
 blockInput.addEventListener("keydown", (e) => {
-	if(e.which == 13) {
-		chrome.runtime.sendMessage({directive: "addSite", site: blockInput.value}, () => {
-			createEl(blockInput.value);
-			blockInput.value = "";
-		});
-	}
+    if(e.which == 13) {
+        chrome.runtime.sendMessage({directive: "addSite", site: blockInput.value}, () => {
+            createEl(blockInput.value);
+            blockInput.value = "";
+        });
+    }
 });
 
 let createEl = (site) => {
-	let siteEl = document.createElement("li");
-	let text = document.createTextNode("× " + site);
-	siteEl.appendChild(text);
-	sitesEl.appendChild(siteEl);
-	siteEl.onclick = () => {
-		chrome.runtime.sendMessage({directive: "removeSite", site});
-		siteEl.parentNode.removeChild(siteEl);
-	};
+    let siteEl = document.createElement("li");
+    let text = document.createTextNode("× " + site);
+    siteEl.appendChild(text);
+    sitesEl.appendChild(siteEl);
+    siteEl.onclick = () => {
+        chrome.runtime.sendMessage({directive: "removeSite", site});
+        siteEl.parentNode.removeChild(siteEl);
+    };
 };
 
-chrome.storage.sync.get("state", (data) => {
-	renderToggle(data.state.enabled);
-});
+window.onload = function() {
+    chrome.storage.sync.get("state", (data) => {
+        renderToggle(data.state.enabled);
+    });
+};
