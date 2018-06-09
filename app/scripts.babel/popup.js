faa5f349af300deeb9707256c0c445443d9612cf
tarnas14 <tarnas14@gmail.com>
2018-06-10 01:13:59 +0200

    removed lint errors due to "

--- app/scripts.babel/popup.js
+++ app/scripts.babel/popup.js
@@ -1,56 +1,56 @@
-"use strict";
+'use strict';
 
-let toggleButton = document.querySelector("#toggle");
-let blockListButton = document.querySelector("#blocklist");
-let mainView = document.querySelector("#main");
-let blockView = document.querySelector("#blockView");
-let blockInput = document.querySelector("#blockInput");
-let sitesEl = document.querySelector("#sites");
+let toggleButton = document.querySelector('#toggle');
+let blockListButton = document.querySelector('#blocklist');
+let mainView = document.querySelector('#main');
+let blockView = document.querySelector('#blockView');
+let blockInput = document.querySelector('#blockInput');
+let sitesEl = document.querySelector('#sites');
 
 toggleButton.onclick = () => {
-    chrome.runtime.sendMessage({directive: "togglePigeon"}, (response) => {
+    chrome.runtime.sendMessage({directive: 'togglePigeon'}, (response) => {
         renderToggle(response);
     });
 };
 
 let renderToggle = (toggled) => {
-    toggleButton.innerHTML = (toggled ? "Turn Off" : "Turn On");
-    toggleButton.style.backgroundColor = (toggled ? "#000000" : "#FFFFFF");
-    toggleButton.style.color = (toggled ? "#FFFFFF" : "#000000");
+    toggleButton.innerHTML = (toggled ? 'Turn Off' : 'Turn On');
+    toggleButton.style.backgroundColor = (toggled ? '#000000' : '#FFFFFF');
+    toggleButton.style.color = (toggled ? '#FFFFFF' : '#000000');
 };
 
 blockListButton.onclick = () => {
-    mainView.style.display = "none";
-    blockView.style.display = "";
-    chrome.storage.sync.get("state", (data) => {
-        sitesEl.innerHTML = "";
+    mainView.style.display = 'none';
+    blockView.style.display = '';
+    chrome.storage.sync.get('state', (data) => {
+        sitesEl.innerHTML = '';
         let sites = data.state.urls;
         sites.forEach(createEl);
     });
 };
 
-blockInput.addEventListener("keydown", (e) => {
+blockInput.addEventListener('keydown', (e) => {
     if(e.which == 13) {
-        chrome.runtime.sendMessage({directive: "addSite", site: blockInput.value}, () => {
+        chrome.runtime.sendMessage({directive: 'addSite', site: blockInput.value}, () => {
             createEl(blockInput.value);
-            blockInput.value = "";
+            blockInput.value = '';
         });
     }
 });
 
 let createEl = (site) => {
-    let siteEl = document.createElement("li");
-    let text = document.createTextNode("× " + site);
+    let siteEl = document.createElement('li');
+    let text = document.createTextNode('× ' + site);
     siteEl.appendChild(text);
     sitesEl.appendChild(siteEl);
     siteEl.onclick = () => {
-        chrome.runtime.sendMessage({directive: "removeSite", site});
+        chrome.runtime.sendMessage({directive: 'removeSite', site});
         siteEl.parentNode.removeChild(siteEl);
     };
 };
 
 window.onload = function() {
-    chrome.storage.sync.get("state", (data) => {
+    chrome.storage.sync.get('state', (data) => {
         renderToggle(data.state.enabled);
     });
 };
